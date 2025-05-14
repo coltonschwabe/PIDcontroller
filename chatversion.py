161397c4564ede50adc81a8566a92ba4834b81d8
Colton Schwabe <coltonschwabe@Coltons-MacBook-Air-8.local>
2025-05-14 11:31:00 -0400

    with labels

--- chatversion.py
+++ chatversion.py
@@ -33,13 +33,15 @@ closed_loop = ctrl.feedback(controller * temp_system, 1, -1) #system1, system2, 
 #time, response = ctrl.step_response(closed_loop, np.linspace(0,60,500))
 times = np.linspace(0, 60*4, 500*4)
 dt = times[1] - times[0]
-vals = np.concatenate((np.full(500, 19.50), np.full(500, 20.5), np.full(500, 19.5), np.full(500, 20.5)))
+vals = np.concatenate((np.full(500, 13.50), np.full(500, 26.5), np.full(500, 13.5), np.full(500, 26.5)))
 
 state = 0
 
 response = []
+response2 = []
 
 integral = 0
+integralTerm = 0
 
 currentresp = 0 # current response
 
@@ -48,18 +50,14 @@ currentresp = 0 # current response
 max = 150000
 
 def sat(val, m):
-    if (val > m): 
-        #print(val)
-        return m
-    elif (val < -1*m):
-        #print(val) 
-        return -1*m
+    if (val > m): return m
+    elif (val < -1*m): return -1*m
 
     return val
 
 for i in range(0, len(times)-1):
     error = vals[i] - currentresp
-    integral += error * dt
+    
     PIDval = K/Ti * integral + K * error
 
     PIDvalsat = sat(PIDval, max) # saturates output
@@ -68,8 +66,29 @@ for i in range(0, len(times)-1):
     currentresp = np.squeeze(r[-1])
     response.append(currentresp)
 
+    integral += error * dt
+
     state = np.squeeze(s[:,-1])
 
-plt.plot(times[:-1], response)
-plt.plot(times, vals)
+state = 0
+currentresp = 0
+for i in range(0, len(times)-1):
+    error = vals[i] - currentresp
+    
+    PIDval_unsat = integralTerm + K * error
+    PIDvalsat = sat(PIDval_unsat, max) # saturates output
+
+    t, r, s = ctrl.forced_response(temp_system, [times[i], times[i + 1]], [PIDvalsat, PIDvalsat], X0=state, return_x = True) #time, response
+    currentresp = np.squeeze(r[-1])
+    response2.append(currentresp)
+
+    integralTerm += (K/Ti * error + (1/(Ti)) * (PIDvalsat - PIDval_unsat)) * dt
+
+    state = np.squeeze(s[:,-1])
+
+
+plt.plot(times[:-1], response, label = 'Without Anti-windup')
+plt.plot(times[:-1], response2, label = 'With Anti-windup')
+plt.plot(times, vals, label = 'Setpoint')
+plt.legend()
 plt.show()
